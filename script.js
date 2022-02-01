2eb20f924784e50c178fa89ee062325e6e156745
hsimic <hrvoje.simic@gmail.com>
2022-02-01 11:18:25 +0100

    rezervacija Sanja iz Češke

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const monthNames = {
   en: {5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October"}
 };
 const reservations = [
-  ["2022-06-22", "2022-06-27"], // mi
+  ["2022-06-22", "2022-06-26"], // mi
+  ["2022-06-26", "2022-07-02"], // Sanja iz Češke
   ["2022-07-02", "2022-07-09"],
 ];
 
